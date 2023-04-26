4e042613b425eb9280564c00a30c02db9d9804c1
brainroot <sysadmin@brainroot.ru>
2023-04-26 18:04:00 +0300

    Jenkinsfile_test_build_4

--- test/config.test.cpp
+++ test/config.test.cpp
@@ -153,7 +153,6 @@ TEST_F(TLoadConfigTest, LoadConfig)
     EXPECT_NO_THROW(LoadConfig(config,
                                TestRootDir + "/test_config.json",
                                TestRootDir + "/classes",
-                               TestRootDir + "/builtin_classes",
                                SchemaFile,
                                ClassSchemaFile));
 
